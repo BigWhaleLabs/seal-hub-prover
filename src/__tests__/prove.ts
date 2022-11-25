024645cdd03229478972b1dc34bd611986f08d5f
ruslan zapevalov <ruslan@bigwhalelabs.com>
2022-11-25 13:19:04 +0400

    fix test

--- src/__tests__/prove.ts
+++ src/__tests__/prove.ts
@@ -1,9 +1,11 @@
+import * as JSONbig from 'json-bigint'
 import * as request from 'supertest'
 import * as shutdown from 'http-graceful-shutdown'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { Mongoose } from 'mongoose'
 import { Server } from 'http'
 import { Wallet } from 'ethers'
+import generateInput from '@/__tests__/helpers/generateProof'
 import runApp from '@/helpers/runApp'
 import runMongo from '@/helpers/runMongo'
 
@@ -38,9 +40,20 @@ describe('Prove endpoint', () => {
     const message = 'Signature for SealHub'
     const wallet = Wallet.createRandom()
     const signature = await wallet.signMessage(message)
+    const input = await generateInput(signature, message)
+    const data = {} as {
+      TPreComputes: string
+      U: string
+      s: string
+    }
+    Object.entries(input).forEach(([key, value]) => {
+      data[key as keyof typeof data] = JSONbig.stringify(value)
+    })
     await request(server)
       .post('/prove')
-      .send({ signature: signature, message })
+      .attach('U', Buffer.from(data.U))
+      .attach('TPreComputes', Buffer.from(data.TPreComputes))
+      .attach('s', Buffer.from(data.s))
       .expect(200)
   })
 })
