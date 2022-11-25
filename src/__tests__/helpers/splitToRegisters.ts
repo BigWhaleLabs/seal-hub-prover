0808627c0a75b0b803bba54f7d3215d8336f80b7
ruslan zapevalov <ruslan@bigwhalelabs.com>
2022-11-25 13:44:01 +0400

    fix curlies

--- src/__tests__/helpers/splitToRegisters.ts
+++ src/__tests__/helpers/splitToRegisters.ts
@@ -9,9 +9,8 @@ function addHexPrefix(str: string) {
 export default function (value?: BN | string) {
   const registers = [] as bigint[]
 
-  if (!value) {
-    return [0n, 0n, 0n, 0n]
-  }
+  if (!value) return [0n, 0n, 0n, 0n]
+
   const hex = value.toString('hex').padStart(64, '0')
   for (let k = 0; k < REGISTERS; k++) {
     // 64bit = 16 chars in hex
