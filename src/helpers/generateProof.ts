aa700eb8bd5ebe6583ed57298391fe4d2688f6ba
Michel Erokhov <mixaileroxov@gmail.com>
2023-01-31 19:45:03 +0300

    fix filepath

--- src/helpers/generateProof.ts
+++ src/helpers/generateProof.ts
@@ -3,8 +3,6 @@ import ProofInput from '@/models/ProofInput'
 import ProofResult from '@/models/ProofResult'
 
 export default async function (input: ProofInput) {
-  const base = process.cwd()
-
   const ecdsaResult: ProofResult = await snarkjs.groth16.fullProve(
     {
       U: input.U,
@@ -13,16 +11,16 @@ export default async function (input: ProofInput) {
       TPrecomputes: input.TPrecomputes,
       T: input.T,
     },
-    `${base}/zk/ECDSAChecker.wasm`,
-    `${base}/zk/ECDSAChecker_final.zkey`
+    './zk/ECDSAChecker.wasm',
+    './zk/ECDSAChecker_final.zkey'
   )
   const uPrecomputesResult: ProofResult = await snarkjs.groth16.fullProve(
     {
       U: input.U,
       rInv: input.rInv,
     },
-    `${base}/zk/UPrecomputesChecker.wasm`,
-    `${base}/zk/UPrecomputesChecker_final.zkey`
+    './zk/UPrecomputesChecker.wasm',
+    './zk/UPrecomputesChecker_final.zkey'
   )
   return {
     ecdsaResult,
